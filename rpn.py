ba28867a49d60f7a63ad890343e3efc66713142f
hangould <hegould@umich.edu>
2017-11-03 14:26:43 -0400

    added color functionality and quit option

--- rpn.py
+++ rpn.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 import operator
+import readline
+import sys
+import colored
+from colored import fg, bg, attr, stylize
 
 ops = {
         '+': operator.add,
         '-': operator.sub,
         '^': operator.pow
 }
+grey = colored.fg("grey_54") 
+blue = colored.fg("sky_blue_2")
+green = colored.fg("pale_green_3b")
+red = colored.fg("indian_red_1c")
+orange = colored.fg("sandy_brown")
 
 def calculate(myarg):
     stack = list()
@@ -18,12 +27,29 @@ def calculate(myarg):
             function = ops[token]
             result = function(arg1, arg2)
             stack.append(result)
-    print(stack)
+    print(stylize("[", grey), end="")
+    for val in stack:
+        if val < 0 and val % 2 == 1:
+            print(stylize(val, orange), ", ", end="")
+        elif val < 0 and val % 2 != 1:
+            print(stylize(val, red), ", ", end="")
+        elif val >= 0 and val % 2 == 1:
+            print(stylize(val, green), ", ", end="")
+        else:
+            print(stylize(val, blue), ", ", end="")
+    print(stylize("]\n", grey), end="")
     return stack.pop()
 
 def main():
+    angry = colored.fg("red") + colored.attr("bold")
+    print ('%s Hello World !!! %s' % (fg(1), attr(0)))
+    print(stylize("This is VERY angry text.", angry, colored.attr("underlined")))
+    print(stylize("This is green.", colored.fg("green")))
     while True:
-        calculate(input("rpn calc> "))
+        val = input("rpn calc> ")
+        if val == 'q':
+            return
+        calculate(val)
 
 if __name__ == '__main__':
     main()
